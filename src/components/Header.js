3828a191c4626e4401017cac0c8c065521d309ab
Anton Savelyev <tensor@antons-mbp.kpn>
2023-02-13 15:50:14 +0100

    Added meta tags to index.html file

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <img src="/my-app/src/images/Logo.jpg"></img>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a></a>
+                </li>
+                <li></li>
+                    <a></a>
+                <li></li>
+                    <a></a>
+                <li></li>
+                    <a></a>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
